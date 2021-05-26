d335d7dfa4e77e8d5e9379bddbe2260bcde44ad4
anggadsa <angga.dsatria07@gmail.com>
2021-05-26 20:03:09 +0700

    Update

--- test.js
+++ test.js
@@ -1,25 +1,87 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
+//Start of Image Uploader
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
+var Busboy = require('busboy');
+const {Storage} = require('@google-cloud/storage');
+const storage = new Storage();
+const uuidv1 = require('uuidv1');
+
+exports.uploadFile = (req, res) => {
+  if (req.method === 'POST') {
+    var busboy = new Busboy({ headers: req.headers });
+    const tmpdir = os.tmpdir();
+    const fields = {};
+    const uploads = {};
+
+    busboy.on('field', (fieldname, val) => {
+      fields[fieldname] = val;
+    });
+
+    busboy.on('file', (fieldname, file, filename) => {
+      if (fieldname === 'image') {
+        console.log(`Processed file ${filename}`);
+        const filepath = path.join(tmpdir, filename);
+        uploads[fieldname] = filepath;
+        file.pipe(fs.createWriteStream(filepath));
+      }
+    });
+
+    busboy.on('error', (error) => {
+      console.log(error)
+    })
+
+    busboy.on('finish', () => {
+
+      const userId = fields["user_id"];
+      const imageId = uuidv1();
+
+      if (userId === undefined) {
+        res.status(400);
+        res.send({error: 'user_id is not provided'});
+        console.log(new Error('user_id is not provided'))
+      }
+
+      // for (const name in uploads) {
+      const file = uploads["image"];
+      if (file === undefined) {
+        res.status(400);
+        res.send({error: 'image is not provided'});
+        console.log(new Error('image is not provided'))
+      }
+
+      var fileExtensionArr = file.split(".");
+      var fileExtension = fileExtensionArr[fileExtensionArr.length-1];
+
+      const destination = `${userId}/${imageId}.${fileExtension}`;
+      const options = {
+        destination: destination
+      };
+      //uploading to cloud storage
+      const bucketName = "image-upload-capstone-project"
+      storage
+      .bucket(bucketName)
+      .upload(file, options)
+      .then(() => {
+        console.log(`${file} uploaded to gs://${bucketName}/${destination}`);
+        fs.unlinkSync(file);
+        const data = {
+          operation_id: imageId,
+          path: `gs://${bucketName}/${destination}`
+        };
+        res.send(data);
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+        res.status(500).send(err)
       });
- });
+    });
+
+    busboy.end(req.rawBody)
+    req.pipe(busboy);
   } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+    res.status(405).end();
   }
-}).listen(8080);
+};
+//End of Image Uploader
